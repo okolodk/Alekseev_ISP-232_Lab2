688b915caaf77e848422d9cc65a4b4306c635ef4
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-07 10:18:15 +0300

    Задание 5

--- Program.cs
+++ Program.cs
@@ -26,7 +26,11 @@
             //int age = Convert.ToInt32(Console.ReadLine());
             //if (age >= 18) { Console.WriteLine("Доступ разрешен"); }
             //else { Console.WriteLine("Доступ запрещен"); }
-
+            //Задание 5
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //if (num1 > num2) { Console.WriteLine(num2); }
+            //else { Console.WriteLine(num1); }
         }
     }
 }

46bf7a63eda1693b33a629012bc74cc2acd53cfe
Aleckseev Grisha <grisha.zhivot@bk.ru>
2025-09-07 09:43:18 +0300

    Задание 2

--- Program.cs
+++ Program.cs
@@ -6,9 +6,13 @@
         {
             //Задание 1
             //int number = Convert.ToInt32(Console.ReadLine());
-            //if (number % 2 == 0 ) { Console.WriteLine("Чётное"); }
-            //else { Console.WriteLine("Не чётное"); }
-            
+            //if (number % 2 == 0 ) { Console.WriteLine($"Число {number} чётное"); }
+            //else { Console.WriteLine($"Число {} нечётное"); }
+            //Задание 2
+            //string password = Console.ReadLine();
+            //string conf_password = Console.ReadLine();
+            //if (password == conf_password) { Console.WriteLine("Пароль принят"); }
+            //else { Console.WriteLine("Пароль не принят"); }
 
         }
     }
